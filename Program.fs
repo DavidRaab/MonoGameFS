de7cb294aa89a2ed1a11bd6e75040c58c9f39866
David Raab <davidraab83@gmail.com>
2022-08-19 21:30:30 +0200

    Changed namespace/module definition
    This way, rest of code can be flat instead of indented.

--- Program.fs
+++ Program.fs
@@ -1,8 +1,9 @@
-namespace MyGame
+module MyGame.App
 open System
 open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
 open Microsoft.Xna.Framework.Input
+
 
 type Fonts = {
     Default: SpriteFont
@@ -24,69 +25,70 @@ module GameState =
 
 type MyGame = Game1<Assets,GameState>
 
-module App =
-    let init (game:MyGame) =
-        game.SetResolution 854 480
-        GameState.create ()
 
-    let loadContent (game:MyGame) = {
-            Font = {
-                Default = game.Content.Load<SpriteFont>("Font")
-            }
-            Texture = {
-                Background = game.Content.Load<Texture2D>("example")
-            }
+let init (game:MyGame) =
+    game.SetResolution 854 480
+    GameState.create ()
+
+let loadContent (game:MyGame) = {
+        Font = {
+            Default = game.Content.Load<SpriteFont>("Font")
         }
+        Texture = {
+            Background = game.Content.Load<Texture2D>("example")
+        }
+    }
 
-    let update (model:GameState) (gameTime:GameTime) (game:MyGame) =
-        FPS.update gameTime
-        //printfn "%A" FPS.state
+let update (model:GameState) (gameTime:GameTime) (game:MyGame) =
+    FPS.update gameTime
+    //printfn "%A" FPS.state
 
-        let gamePad  = GamePad.GetState PlayerIndex.One
-        let keyboard = Keyboard.GetState ()
+    let gamePad  = GamePad.GetState PlayerIndex.One
+    let keyboard = Keyboard.GetState ()
 
-        // Vibration through Triggers
-        printfn "%f %f" gamePad.Triggers.Left gamePad.Triggers.Right
-        GamePad.SetVibration(0,
-            gamePad.Triggers.Left,
-            gamePad.Triggers.Right
-        ) |> ignore
+    // Vibration through Triggers
+    printfn "%f %f" gamePad.Triggers.Left gamePad.Triggers.Right
+    GamePad.SetVibration(0,
+        gamePad.Triggers.Left,
+        gamePad.Triggers.Right
+    ) |> ignore
 
-        if keyboard.IsKeyDown Keys.Space then
-            ignore <| GamePad.SetVibration(0, 1.0f, 1.0f)
+    if keyboard.IsKeyDown Keys.Space then
+        ignore <| GamePad.SetVibration(0, 1.0f, 1.0f)
 
-        if GamePad.isPressed gamePad.Buttons.A then
-            printfn "Pressed A"
+    if GamePad.isPressed gamePad.Buttons.A then
+        printfn "Pressed A"
 
-        if GamePad.isPressed gamePad.Buttons.Back || keyboard.IsKeyDown Keys.Escape then
-            game.Exit()
+    if GamePad.isPressed gamePad.Buttons.Back || keyboard.IsKeyDown Keys.Escape then
+        game.Exit()
 
-        model
+    model
 
-    let draw (model:GameState) (gameTime:GameTime) (game:MyGame) =
-        let width, height = game.GetResolution ()
+let draw (model:GameState) (gameTime:GameTime) (game:MyGame) =
+    let width, height = game.GetResolution ()
 
-        game.spriteBatch.Draw(
-            game.Asset.Texture.Background,
-            Rectangle(0, 0, width, height),
-            Color.White
-        )
+    game.spriteBatch.Draw(
+        game.Asset.Texture.Background,
+        Rectangle(0, 0, width, height),
+        Color.White
+    )
 
-        FPS.draw game.Asset.Font.Default game.spriteBatch
+    FPS.draw game.Asset.Font.Default game.spriteBatch
 
-        game.spriteBatch.DrawString(
-            game.Asset.Font.Default,
-            "Hello, World!",
-            Vector2(100f, 100f),
-            Color.White
-        )
+    game.spriteBatch.DrawString(
+        game.Asset.Font.Default,
+        "Hello, World!",
+        Vector2(100f, 100f),
+        Color.White
+    )
 
-    // Run MonoGame Application
-    [<EntryPoint;STAThread>]
-    let main argv =
-        using (new Game1<Assets,GameState>(init, loadContent, update, draw)) (fun game ->
-            game.Content.RootDirectory <- "Content"
-            game.IsMouseVisible        <- true
-            game.Run()
-        )
-        1
+
+// Run MonoGame Application
+[<EntryPoint;STAThread>]
+let main argv =
+    using (new Game1<Assets,GameState>(init, loadContent, update, draw)) (fun game ->
+        game.Content.RootDirectory <- "Content"
+        game.IsMouseVisible        <- true
+        game.Run()
+    )
+    1
