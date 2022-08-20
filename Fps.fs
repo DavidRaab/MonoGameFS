49d47e76eabc89754a140b94f2382d2262ddd28a
David Raab <davidraab83@gmail.com>
2022-08-20 04:05:32 +0200

    Changed namespace/module definiton
    Additionally added scaling of FPS Text.

--- Fps.fs
+++ Fps.fs
@@ -1,4 +1,4 @@
-namespace MyGame
+module MyGame.FPS
 open System
 open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
@@ -9,32 +9,36 @@ type FPS = {
     mutable FPS:         float
 }
 
-module FPS =
-    let create frames time fps = {
-        Frames      = frames
-        ElapsedTime = time
-        FPS         = fps
-    }
+let create frames time fps = {
+    Frames      = frames
+    ElapsedTime = time
+    FPS         = fps
+}
 
-    // Global State
-    let state =
-        create 0 (TimeSpan 0) 0.0
+// Global State
+let state =
+    create 0 (TimeSpan 0) 0.0
 
-    // Called on each update
-    let update (gameTime:GameTime) =
-        state.Frames      <- state.Frames + 1
-        state.ElapsedTime <- state.ElapsedTime + gameTime.ElapsedGameTime
+// Called on each update
+let update (gameTime:GameTime) =
+    state.Frames      <- state.Frames + 1
+    state.ElapsedTime <- state.ElapsedTime + gameTime.ElapsedGameTime
 
-        if state.ElapsedTime.TotalSeconds >= 1.0 then
-            state.FPS         <- float state.Frames / state.ElapsedTime.TotalSeconds
-            state.Frames      <- 0
-            state.ElapsedTime <- TimeSpan 0
+    if state.ElapsedTime.TotalSeconds >= 1.0 then
+        state.FPS         <- float state.Frames / state.ElapsedTime.TotalSeconds
+        state.Frames      <- 0
+        state.ElapsedTime <- TimeSpan 0
 
-    let draw font (sb:SpriteBatch) =
-        sb.DrawString(
-            font,
-            String.Format("FPS: {0:0.00}", state.FPS),
-            Vector2(0f, 0f),
-            Color.Yellow
-        )
+let draw font (sb:SpriteBatch) =
+    sb.DrawString(
+        spriteFont = font,
+        text       = String.Format("FPS: {0:0.00}", state.FPS),
+        position   = Vector2(3f, 3f),
+        color      = Color.Yellow,
+        rotation   = 0f,
+        origin     = Vector2.Zero,
+        scale      = 1.33f,
+        effects    = SpriteEffects.None,
+        layerDepth = 0f
+    )
 
